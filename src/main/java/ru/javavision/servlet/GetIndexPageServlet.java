324a8b0228bf8f4c2f8b22415b774e6c4a910779
ivan <iizdepskyy112@gmail.com>
2019-09-06 15:33:19 +0300

    Java Servlet Life Cycle

---
+++ src/main/java/ru/javavision/servlet/GetIndexPageServlet.java
@@ -0,0 +1,40 @@
+package ru.javavision.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetIndexPageServlet extends HttpServlet {
+
+    private final static String index = "/WEB-INF/view/index.jsp";
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("*************SERVLET IS INIT************");
+        System.out.println("FOR PATH '/' WILL RENDER VIEW : " + index);
+    }
+    /*
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+        super.init(config);
+    }
+    */
+
+    /**
+     * Multithreading scope.
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        System.out.println("doGet is work!");
+        req.getRequestDispatcher(index).forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("*************SERVLET IS DESTROY************");
+    }
+}
